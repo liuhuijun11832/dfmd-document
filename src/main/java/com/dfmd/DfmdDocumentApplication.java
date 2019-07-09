5aaa5b0ff545b4ade63bb2aec2220cf3edc077de
liuhuijun <liuhuijun2017@gmail.com>
2019-07-09 18:03:26 +0800

    项目初始化。

---
+++ src/main/java/com/dfmd/DfmdDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.dfmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfmdDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfmdDocumentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dfmd/DfmdDocumentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dfmd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DfmdDocumentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
