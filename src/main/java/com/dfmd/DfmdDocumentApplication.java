a451070b45800eb9b74269b0d509123b55d9275b
liuhuijun <liuhuijun_2017@163.com>
2019-07-16 21:57:37 +0800

    派出Srping boot Security的自动装配类。

--- src/main/java/com/dfmd/DfmdDocumentApplication.java
+++ src/main/java/com/dfmd/DfmdDocumentApplication.java
@@ -1,12 +1,13 @@
 package com.dfmd;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan("com.dfmd.mapper")
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class DfmdDocumentApplication {
 
     public static void main(String[] args) {
